4d082ba3f5c25804c46ae57d41c551f3560217da
adamesh <adameshh@gmail.com>
2017-06-08 09:23:54 -0400

    #get_first_name_of_season_winner completed

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,45 @@
+require 'pry'
+# {
+#   "season 30": [
+#     {
+#       "name":      "Beth Smalls",
+#       "age":       "26",
+#       "hometown":  "Great Falls, Virginia",
+#       "occupation":"Nanny/Freelance Journalist",
+#       "status":    "Winner"
+#     },
+#     {
+#       "name":       "Becca Tilley",
+#       "age":        "27",
+#       "hometown":   "Shreveport, Louisiana",
+#       "occupation": "Chiropractic Assistant",
+#       "status":     "Eliminated Week 8"
+#     }
+#   ],
+#   "season 29": [
+#     {
+#       "name":      "Ashley Yeats",
+#       "age":       "24",
+#       "hometown":  "Denver, Colorado",
+#       "occupation":"Dental Assitant",
+#       "status":    "Winner"
+#     },
+#     {
+#       "name":       "Sam Grover",
+#       "age":        "29",
+#       "hometown":   "New York, New York",
+#       "occupation": "Entertainer",
+#       "status":     "Eliminated Week 6"
+#     }
+#   ]
+# }
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contenstant|
+    if contenstant['status'] == 'Winner'
+      return contenstant['name'].split[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
